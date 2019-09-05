c0602f5f903349d8dd7729ef339a46e2fbd21cba
Matthew Adams <matthew.adams@endjin.com>
2019-09-05 14:13:07 +0100

    Fixed bug: Set RootTenant.Id correctly.

--- Solutions/Corvus.Tenancy.Abstractions/Corvus/Tenancy/Internal/RootTenant.cs
+++ Solutions/Corvus.Tenancy.Abstractions/Corvus/Tenancy/Internal/RootTenant.cs
@@ -18,6 +18,7 @@ namespace Corvus.Tenancy
         public RootTenant(IJsonSerializerSettingsProvider serializerSettingsProvider)
             : base(serializerSettingsProvider)
         {
+            this.Id = "Root";
         }
     }
 }
